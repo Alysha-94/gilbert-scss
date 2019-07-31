7b180c0e8f0dea60bfb1ad325760f9dfc7fe8ceb
Alysha-94 <alysha.iannetta@gmail.com>
2019-07-31 13:25:09 +1000

    add gilbert scss for plugin

---
+++ gilbert/plugins/scss.py
@@ -0,0 +1,27 @@
+import sass
+
+from gilbert import Site
+from gilbert.content import Content, Renderable
+from gilbert.utils import oneshot
+
+
+class SCSS(Renderable, Content):
+    output_extension: str = 'css'
+    scss_options: dict = {}
+
+    @oneshot
+    def content(self):
+        options = self.scss_options
+        if not options:
+            options = self.site.config.get('content_type', {}).get('SCSS', {})
+
+        return sass.compile(string=self.data, **options)
+
+
+def load_scss(path):
+    data = path.read_text(encoding='utf-8')
+
+    return data, {'content_type': 'SCSS'}
+
+
+Site.register_loader('scss', load_scss)
