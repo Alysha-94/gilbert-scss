2a0030114572810a8f3f16dbc20b2e653e1b383e
Alysha-94 <alysha.iannetta@gmail.com>
2019-07-31 16:19:52 +1000

    setup long_description as readme file

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup
 
+with open("README.md", "r") as f:
+  long_description = f.read()
+
 setup(name='gilbert-scss',
       version='0.0.1',
       description='A plugin for Gilbert to render content using libsass',
+      long_description= long_description,
       url='https://github.com/Alysha-94/gilbert-scss',
       author='Alysha Iannetta',
       packages=['gilbert.plugins'],
